8cf0ee72671413eb4bc9e8af58e197cccd3a7e81
MerrikM <merrikm.pl03@gmail.com>
2023-03-19 18:39:40 +0500

    first commit

---
+++ http-env-variables/README.md
@@ -0,0 +1,4 @@
+# HW_1.5 HTTP modules, env variables
+then run index.js with 
+
++ **cmd npm run dev**
---
+++ http-env-variables/config.js
@@ -0,0 +1,3 @@
+// defalut values
+myAPIKey=c3d7902f8f646a78fe5555fd084dd9e2
+url = `http://api.weatherstack.com/current?access_key=c3d7902f8f646a78fe5555fd084dd9e2&query=London`
