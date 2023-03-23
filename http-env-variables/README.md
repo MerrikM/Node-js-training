ac4ff92fa591c3318a112075654985a22efb4628
MerrikM <128287955+MerrikM@users.noreply.github.com>
2023-03-23 23:06:05 +0500

    Delete http-env-variables directory

--- http-env-variables/README.md
+++
@@ -1,4 +0,0 @@
-# HW_1.5 HTTP modules, env variables
-then run index.js with 
-
-+ **cmd npm run dev**
--- http-env-variables/config.js
+++
@@ -1,3 +0,0 @@
-// defalut values
-myAPIKey=c3d7902f8f646a78fe5555fd084dd9e2
-url = `http://api.weatherstack.com/current?access_key=c3d7902f8f646a78fe5555fd084dd9e2&query=London`
